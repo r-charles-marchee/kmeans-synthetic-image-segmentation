a88457bc65733a251a04cd892aed1ed0576665d7
r-charles-marchee <charles.marchee@protonmail.com>
2025-08-07 03:38:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="kmeans-synthetic-image-segmentation",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scikit-learn",
+        "scipy",
+        "scikit-image"
+    ],
+    author="R. Charles Marchee",
+    author_email="rmarchee@capellauniversity.edu",
+    description="Synthetic image segmentation using K-Means clustering",
+    url="https://github.com/yourusername/kmeans-synthetic-image-segmentation",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
